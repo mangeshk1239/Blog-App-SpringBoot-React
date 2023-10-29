f77baadbb9b85fec5e2560cbc09d2dd1c753519c
Mangesh Kamble <mangeshkamble1239@gmail.com>
2023-10-29 21:39:48 +0530

    added user model

---
+++ backend/src/main/java/com/webapp/blog/model/User.java
@@ -0,0 +1,34 @@
+package com.webapp.blog.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+}
