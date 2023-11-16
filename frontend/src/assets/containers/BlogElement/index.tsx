59a58ed55d88b227b4e1198d5ddb71262257d4ef
Mangesh Kamble <mangeshkamble1239@gmail.com>
2023-11-16 17:12:55 +0530

    few changes in the frontend

--- frontend/src/assets/containers/BlogElement/index.tsx
+++ frontend/src/assets/containers/BlogElement/index.tsx
@@ -1,20 +1,36 @@
 import * as React from "react";
+import { IBlog } from "./interfaces";
 
 export default function BlogElement(): JSX.Element {
     React.useEffect(() => {
         handleFetchBlogs();
     }, []);
 
+    const [getBlogs, setBlogs] = React.useState<Array<IBlog>>([]);
+
     return (
         <>
-            hiiiii
+            {
+                getBlogs.length > 0 ?
+                    getBlogs.map((blog) => {
+                        return <>
+                            {blog.title}
+                            <br />
+                            {blog.description}
+                            <br />
+                            <br />
+                            <br />
+                        </>
+                    }) :
+                    <>NO BLOGS...</>
+            }
         </>
     )
 
     async function handleFetchBlogs() {
         try {
             const response = await fetch("/api/blogs/fetch").then(response => response.json());
-            console.log("response", response);
+            if (response.success) setBlogs(response.data);
         } catch (error) {
             console.log("ERROR", error);
         }
